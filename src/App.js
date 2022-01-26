673dd3f6d167ac18b5d9dd366292ee63d292b2b1
adoool <adibjadidi74@gmail.com>
2022-01-26 13:23:42 +0330

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import TodoApp from "./components/TodoApp";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{ fontSize: "64px" }}>Just do it</h1>
+      <TodoApp />
+    </div>
+  );
+}
+
+export default App;
